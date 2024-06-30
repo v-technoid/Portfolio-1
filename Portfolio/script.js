4677f534f9f0135ca14b68b4eb4272f47f1e20aa
VISHNUPPRIYAN PL <vishnutechnophile@gmail.com>
2024-07-01 01:24:54 +0530

    Add files via upload Added new feature

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,44 +1,85 @@
-const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+const pageTurnBtn = document.querySelectorAll(".nextprev-btn");
 
 pageTurnBtn.forEach((el, index) => {
-    el.addEventListener('click', () => {
-        const pageTurnId = el.getAttribute('data-page');
+    el.onclick = () => {
+        const pageTurnId = el.getAttribute("data-page");
         const pageTurn = document.getElementById(pageTurnId);
-
-        if (pageTurn.classList.contains('turn')) {
-            pageTurn.classList.remove('turn');
+        if (pageTurn.classList.contains("turn")) {
+            pageTurn.classList.remove("turn");
             setTimeout(() => {
                 pageTurn.style.zIndex = 20 - index;
-            }, 0); 
-        } else {
-            pageTurn.classList.add('turn');
+            }, 500);
+        } 
+        else {
+            pageTurn.classList.add("turn");
             setTimeout(() => {
                 pageTurn.style.zIndex = 20 + index;
-            }, 0);
+            }, 500);
         }
-    });
+    };
 });
 
+const pages = document.querySelectorAll(".book-page.page-right");
+const contact = document.querySelector(".btn.contact-me");
 
+contact.onclick = () => {
+    pages.forEach((page, index) => {
+        setTimeout(() => {
+            page.classList.add("turn");
+            setInterval(() => {
+                page.style.zIndex = 20 + index;
+            }, 500);
+        }, (index + 1) * 200 + 100);
+    });
+};
 
+let totalPages = pages.length;
+let pageNumber = 0;
 
+function reverseIndex() {
+    pageNumber--;
+    if (pageNumber < 0) {
+        pageNumber = totalPages - 1;
+    }
+}
 
+const backProfilesBtn = document.querySelector(".back-profile");
 
+backProfilesBtn.onclick = () => {
+    pages.forEach((_, index) => {
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].classList.remove("turn");
+            setTimeout(() => {
+                reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500);
+        }, (index + 1) * 200 + 100);
+    });
+};
 
+const coverRight = document.querySelector(".cover.cover-right");
+const coverLeft = document.querySelector(".book-page.page-left");
 
+setTimeout(() => {
+    coverRight.classList.add("turn");
+}, 2100);
 
+setTimeout(() => {
+    coverRight.style.zIndex = -1;
+}, 2800);
 
+setTimeout(() => {
+    pageLeft.style.zIndex = 20;
+}, 3200);
+pages.forEach((_, index) => {
+    setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].classList.remove("turn");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].style.zIndex = 10 + index;
+        }, 500);
+    }, (index + 1) * 200 + 2100);
+});
